76268d4e7190549151a813d66fc6939ebe5f0176
Bernardo <bernardo.santos10@gmail.com>
2021-03-11 17:39:54 +0000

    Finished command forming for text mode

--- MP1/xmod.c
+++ MP1/xmod.c
@@ -1,63 +1,204 @@
 #include "xmod.h"
 
-permission_type get_permissions(int oct)
+int make_command_from_text_mode(char *mode, unsigned int *command)
 {
-    permission_type perm;
-    switch (oct)
+    user_type user_t;
+    permission_mode perm_mode;
+    permission_type perm_type = {false, false, false};
+
+    // MODE: user type
+    switch (mode[0])
     {
-    case 00:
-        perm.read = false;
-        perm.write = false;
-        perm.execute = false;
+    case 'u':
+        user_t = OWNER;
         break;
 
-    case 01:
-        perm.read = false;
-        perm.write = false;
-        perm.execute = true;
+    case 'g':
+        user_t = GROUP;
         break;
 
-    case 02:
-        perm.read = false;
-        perm.write = true;
-        perm.execute = false;
+    case 'o':
+        user_t = OTHER;
         break;
 
-    case 03:
-        perm.read = false;
-        perm.write = true;
-        perm.execute = true;
+    case 'a':
+        user_t = ALL;
         break;
 
-    case 04:
-        perm.read = true;
-        perm.write = false;
-        perm.execute = false;
+    default:
+        printf("ERROR: Invalid user type. Should be one of <u|g|o|a>.\n");
+        return -1;
+        break;
+    }
+
+    // MODE: permission mode
+    switch (mode[1])
+    {
+    case '-':
+        perm_mode = REMOVE;
         break;
 
-    case 05:
-        perm.read = true;
-        perm.write = false;
-        perm.execute = true;
+    case '+':
+        perm_mode = ADD;
         break;
 
-    case 06:
-        perm.read = true;
-        perm.write = true;
-        perm.execute = false;
+    case '=':
+        perm_mode = MODIFY;
         break;
 
-    case 07:
-        perm.read = true;
-        perm.write = true;
-        perm.execute = true;
+    default:
+        printf("ERROR: Invalid permission mode. Should be one of <-|+|=>.\n");
+        return -1;
+        break;
+    }
+
+    // MODE: permission type
+    for (int i = 2; i < strlen(mode); i++)
+    {
+        switch (mode[i])
+        {
+        case 'r':
+            perm_type.read = true;
+            break;
+
+        case 'w':
+            perm_type.write = true;
+            break;
+
+        case 'x':
+            perm_type.execute = true;
+            break;
+
+        default:
+            printf("ERROR: Invalid permission type. Should be at least one of <rwx>.\n");
+            return -1;
+            break;
+        }
+    }
+
+    switch (user_t)
+    {
+    case OWNER:
+        //remove current permissions for OWNER
+        if (perm_mode == MODIFY)
+            *command &= 0077;
+
+        if (perm_type.read)
+        {
+            if (perm_mode == REMOVE)
+                *command &= ~S_IRUSR;
+            else
+                *command |= S_IRUSR;
+        }
+        if (perm_type.write)
+        {
+            if (perm_mode == REMOVE)
+                *command &= ~S_IWUSR;
+            else
+                *command |= S_IWUSR;
+        }
+        if (perm_type.execute)
+        {
+            if (perm_mode == REMOVE)
+                *command &= ~S_IXUSR;
+            else
+                *command |= S_IXUSR;
+        }
+
+        break;
+
+    case GROUP:
+        //remove current permissions for GROUP
+        if (perm_mode == MODIFY)
+            *command &= 0707;
+
+        if (perm_type.read)
+        {
+            if (perm_mode == REMOVE)
+                *command &= ~S_IRGRP;
+            else
+                *command |= S_IRGRP;
+        }
+        if (perm_type.write)
+        {
+            if (perm_mode == REMOVE)
+                *command &= ~S_IWGRP;
+            else
+                *command |= S_IWGRP;
+        }
+        if (perm_type.execute)
+        {
+            if (perm_mode == REMOVE)
+                *command &= ~S_IXGRP;
+            else
+                *command |= S_IXGRP;
+        }
+
+        break;
+
+    case OTHER:
+        //remove current permissions for OTHER
+        if (perm_mode == MODIFY)
+            *command &= 0770;
+
+        if (perm_type.read)
+        {
+            if (perm_mode == REMOVE)
+                *command &= ~S_IROTH;
+            else
+                *command |= S_IROTH;
+        }
+        if (perm_type.write)
+        {
+            if (perm_mode == REMOVE)
+                *command &= ~S_IWOTH;
+            else
+                *command |= S_IWOTH;
+        }
+        if (perm_type.execute)
+        {
+            if (perm_mode == REMOVE)
+                *command &= ~S_IXOTH;
+            else
+                *command |= S_IXOTH;
+        }
+
+        break;
+
+    case ALL:
+        //remove current permissions for ALL
+        if (perm_mode == MODIFY)
+            *command &= 0;
+
+        if (perm_type.read)
+        {
+            if (perm_mode == REMOVE)
+                *command &= ~(S_IRUSR | S_IRGRP | S_IROTH);
+            else
+                *command |= S_IRUSR | S_IRGRP | S_IROTH;
+        }
+        if (perm_type.write)
+        {
+            if (perm_mode == REMOVE)
+                *command &= ~(S_IWUSR | S_IWGRP | S_IWOTH);
+            else
+                *command |= S_IWUSR | S_IWGRP | S_IWOTH;
+        }
+        if (perm_type.execute)
+        {
+            if (perm_mode == REMOVE)
+                *command &= ~(S_IXUSR | S_IXGRP | S_IXOTH);
+            else
+                *command |= S_IXUSR | S_IXGRP | S_IXOTH;
+        }
+
         break;
 
     default:
         break;
     }
 
-    return perm;
+    return 0;
 }
 
 int main(int argc, char **argv, char **envp)
@@ -65,167 +206,37 @@ int main(int argc, char **argv, char **envp)
     if (argc < 3)
     {
         printf("Usage:\nxmod [OPTIONS] MODE FILE/DIR\nxmod [OPTIONS] OCTAL-MODE FILE/DIR\n");
-        return 1;
+        return -1;
     }
 
     if (strlen(argv[1]) < 3 || strlen(argv[1]) > 5)
     {
         printf("ERROR: Invalid MODE format. Should be <u|g|o|a><-|+|=><rwx>\n");
-        return 1;
+        return -1;
     }
 
     struct stat buffer;
     if (stat(argv[2], &buffer) != 0)
     {
         perror("ERROR");
-        return 1;
+        return -1;
     }
 
     // current permissions
-    unsigned int curr_mode = buffer.st_mode & 0777;
-    permission_type owner_perms = get_permissions((curr_mode & 0700) >> 6);
-    permission_type group_perms = get_permissions((curr_mode & 0070) >> 3);
-    permission_type other_perms = get_permissions(curr_mode & 0007);
+    unsigned int curr_perm = buffer.st_mode & 0777;
 
-    printf("OWNER - r: %d w: %d x: %d\n", owner_perms.read, owner_perms.write, owner_perms.execute);
-    printf("GROUP - r: %d w: %d x: %d\n", group_perms.read, group_perms.write, group_perms.execute);
-    printf("OTHER - r: %d w: %d x: %d\n", other_perms.read, other_perms.write, other_perms.execute);
+    unsigned int command = curr_perm;
 
-    unsigned int command = 0;
-
-    user_type user_t;
-    permission_mode perm_mode;
-    permission_type perm_type = {false, false, false};
-
-    // MODE: user type
-    switch (argv[1][0])
+    if (make_command_from_text_mode(argv[1], &command) != 0)
     {
-    case 'u':
-        user_t = OWNER;
-        printf("Owner\n");
-        break;
-
-    case 'g':
-        user_t = GROUP;
-        printf("Group\n");
-        break;
-
-    case 'o':
-        user_t = OTHER;
-        printf("Other\n");
-        break;
-
-    case 'a':
-        user_t = ALL;
-        printf("All\n");
-        break;
-
-    default:
-        printf("ERROR: Invalid user type. Should be one of <u|g|o|a>.\n");
-        return 1;
-        break;
-    }
-
-    // MODE: permission mode
-    switch (argv[1][1])
-    {
-    case '-':
-        perm_mode = REMOVE;
-        printf("Remove\n");
-        break;
-
-    case '+':
-        perm_mode = ADD;
-        printf("Add\n");
-        break;
-
-    case '=':
-        perm_mode = MODIFY;
-        printf("Modify\n");
-        break;
-
-    default:
-        printf("ERROR: Invalid permission mode. Should be one of <-|+|=>.\n");
-        return 1;
-        break;
-    }
-
-    // MODE: permission type
-    for (int i = 2; i < strlen(argv[1]); i++)
-    {
-        switch (argv[1][i])
-        {
-        case 'r':
-            perm_type.read = true;
-            printf("Read\n");
-            break;
-
-        case 'w':
-            perm_type.write = true;
-            printf("Write\n");
-            break;
-
-        case 'x':
-            perm_type.execute = true;
-            printf("Execute\n");
-            break;
-
-        default:
-            printf("ERROR: Invalid permission type. Should be at least one of <rwx>.\n");
-            return 1;
-            break;
-        }
-    }
-
-    if (perm_mode != MODIFY)
-        return 1;
-
-    switch (user_t)
-    {
-    case OWNER:
-        if (perm_type.read)
-            command |= S_IRUSR;
-        if (perm_type.write)
-            command |= S_IWUSR;
-        if (perm_type.execute)
-            command |= S_IXUSR;
-        break;
-
-    case GROUP:
-        if (perm_type.read)
-            command |= S_IRGRP;
-        if (perm_type.write)
-            command |= S_IWGRP;
-        if (perm_type.execute)
-            command |= S_IXGRP;
-        break;
-
-    case OTHER:
-        if (perm_type.read)
-            command |= S_IROTH;
-        if (perm_type.write)
-            command |= S_IWOTH;
-        if (perm_type.execute)
-            command |= S_IXOTH;
-        break;
-
-    case ALL:
-        if (perm_type.read)
-            command |= S_IRUSR | S_IRGRP | S_IROTH;
-        if (perm_type.write)
-            command |= S_IWUSR | S_IWGRP | S_IWOTH;
-        if (perm_type.execute)
-            command |= S_IXUSR | S_IXGRP | S_IXOTH;
-        break;
-
-    default:
-        break;
+        printf("Error making new command.\n");
+        return -1;
     }
 
     if (chmod(argv[2], command) != 0)
     {
         perror("chmod() error");
-        return 1;
+        return -1;
     }
 
     return 0;
