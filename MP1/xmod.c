2ae1c06a42c6ae86eabdcbbd41cea9b4ce798f9e
Bernardo <bernardo.santos10@gmail.com>
2021-03-12 00:37:52 +0000

    Add support for octal mode

--- MP1/xmod.c
+++ MP1/xmod.c
@@ -1,223 +1,221 @@
 #include "xmod.h"
 
-int make_command_from_text_mode(char *mode, unsigned int *command)
-{
+int make_command_from_text_mode(char *mode, unsigned int *command) {
     user_type user_t;
     permission_mode perm_mode;
     permission_type perm_type = {false, false, false};
 
     // MODE: user type
-    switch (mode[0])
-    {
-    case 'u':
-        user_t = OWNER;
-        break;
-
-    case 'g':
-        user_t = GROUP;
-        break;
-
-    case 'o':
-        user_t = OTHER;
-        break;
-
-    case 'a':
-        user_t = ALL;
-        break;
-
-    default:
-        printf("ERROR: Invalid user type. Should be one of <u|g|o|a>.\n");
-        return -1;
-        break;
-    }
-
-    // MODE: permission mode
-    switch (mode[1])
-    {
-    case '-':
-        perm_mode = REMOVE;
-        break;
-
-    case '+':
-        perm_mode = ADD;
-        break;
-
-    case '=':
-        perm_mode = MODIFY;
-        break;
-
-    default:
-        printf("ERROR: Invalid permission mode. Should be one of <-|+|=>.\n");
-        return -1;
-        break;
-    }
-
-    // MODE: permission type
-    for (int i = 2; i < strlen(mode); i++)
-    {
-        switch (mode[i])
-        {
-        case 'r':
-            perm_type.read = true;
+    switch (mode[0]) {
+        case 'u':
+            user_t = OWNER;
             break;
 
-        case 'w':
-            perm_type.write = true;
+        case 'g':
+            user_t = GROUP;
             break;
 
-        case 'x':
-            perm_type.execute = true;
+        case 'o':
+            user_t = OTHER;
+            break;
+
+        case 'a':
+            user_t = ALL;
             break;
 
         default:
-            printf("ERROR: Invalid permission type. Should be at least one of <rwx>.\n");
+            printf("ERROR: Invalid user type. ");
+            printf("Should be one of <u|g|o|a>.\n");
             return -1;
             break;
+    }
+
+    // MODE: permission mode
+    switch (mode[1]) {
+        case '-':
+            perm_mode = REMOVE;
+            break;
+
+        case '+':
+            perm_mode = ADD;
+            break;
+
+        case '=':
+            perm_mode = MODIFY;
+            break;
+
+        default:
+            printf("ERROR: Invalid permission mode. ");
+            printf("Should be one of <-|+|=>.\n");
+            return -1;
+            break;
+    }
+
+    // MODE: permission type
+    for (int i = 2; i < strlen(mode); i++) {
+        switch (mode[i]) {
+            case 'r':
+                perm_type.read = true;
+                break;
+
+            case 'w':
+                perm_type.write = true;
+                break;
+
+            case 'x':
+                perm_type.execute = true;
+                break;
+
+            default:
+                printf("ERROR: Invalid permission type. ");
+                printf("Should be at least one of <rwx>.\n");
+                return -1;
+                break;
         }
     }
 
-    switch (user_t)
-    {
-    case OWNER:
-        //remove current permissions for OWNER
-        if (perm_mode == MODIFY)
-            *command &= 0077;
+    switch (user_t) {
+        case OWNER:
+            // remove current permissions for OWNER
+            if (perm_mode == MODIFY)
+                *command &= 0077;
 
-        if (perm_type.read)
-        {
-            if (perm_mode == REMOVE)
-                *command &= ~S_IRUSR;
-            else
-                *command |= S_IRUSR;
-        }
-        if (perm_type.write)
-        {
-            if (perm_mode == REMOVE)
-                *command &= ~S_IWUSR;
-            else
-                *command |= S_IWUSR;
-        }
-        if (perm_type.execute)
-        {
-            if (perm_mode == REMOVE)
-                *command &= ~S_IXUSR;
-            else
-                *command |= S_IXUSR;
-        }
+            if (perm_type.read) {
+                if (perm_mode == REMOVE)
+                    *command &= ~S_IRUSR;
+                else
+                    *command |= S_IRUSR;
+            }
+            if (perm_type.write) {
+                if (perm_mode == REMOVE)
+                    *command &= ~S_IWUSR;
+                else
+                    *command |= S_IWUSR;
+            }
+            if (perm_type.execute) {
+                if (perm_mode == REMOVE)
+                    *command &= ~S_IXUSR;
+                else
+                    *command |= S_IXUSR;
+            }
 
-        break;
+            break;
 
-    case GROUP:
-        //remove current permissions for GROUP
-        if (perm_mode == MODIFY)
-            *command &= 0707;
+        case GROUP:
+            // remove current permissions for GROUP
+            if (perm_mode == MODIFY)
+                *command &= 0707;
 
-        if (perm_type.read)
-        {
-            if (perm_mode == REMOVE)
-                *command &= ~S_IRGRP;
-            else
-                *command |= S_IRGRP;
-        }
-        if (perm_type.write)
-        {
-            if (perm_mode == REMOVE)
-                *command &= ~S_IWGRP;
-            else
-                *command |= S_IWGRP;
-        }
-        if (perm_type.execute)
-        {
-            if (perm_mode == REMOVE)
-                *command &= ~S_IXGRP;
-            else
-                *command |= S_IXGRP;
-        }
+            if (perm_type.read) {
+                if (perm_mode == REMOVE)
+                    *command &= ~S_IRGRP;
+                else
+                    *command |= S_IRGRP;
+            }
+            if (perm_type.write) {
+                if (perm_mode == REMOVE)
+                    *command &= ~S_IWGRP;
+                else
+                    *command |= S_IWGRP;
+            }
+            if (perm_type.execute) {
+                if (perm_mode == REMOVE)
+                    *command &= ~S_IXGRP;
+                else
+                    *command |= S_IXGRP;
+            }
 
-        break;
+            break;
 
-    case OTHER:
-        //remove current permissions for OTHER
-        if (perm_mode == MODIFY)
-            *command &= 0770;
+        case OTHER:
+            // remove current permissions for OTHER
+            if (perm_mode == MODIFY)
+                *command &= 0770;
 
-        if (perm_type.read)
-        {
-            if (perm_mode == REMOVE)
-                *command &= ~S_IROTH;
-            else
-                *command |= S_IROTH;
-        }
-        if (perm_type.write)
-        {
-            if (perm_mode == REMOVE)
-                *command &= ~S_IWOTH;
-            else
-                *command |= S_IWOTH;
-        }
-        if (perm_type.execute)
-        {
-            if (perm_mode == REMOVE)
-                *command &= ~S_IXOTH;
-            else
-                *command |= S_IXOTH;
-        }
+            if (perm_type.read) {
+                if (perm_mode == REMOVE)
+                    *command &= ~S_IROTH;
+                else
+                    *command |= S_IROTH;
+            }
+            if (perm_type.write) {
+                if (perm_mode == REMOVE)
+                    *command &= ~S_IWOTH;
+                else
+                    *command |= S_IWOTH;
+            }
+            if (perm_type.execute) {
+                if (perm_mode == REMOVE)
+                    *command &= ~S_IXOTH;
+                else
+                    *command |= S_IXOTH;
+            }
 
-        break;
+            break;
 
-    case ALL:
-        //remove current permissions for ALL
-        if (perm_mode == MODIFY)
-            *command &= 0;
+        case ALL:
+            // remove current permissions for ALL
+            if (perm_mode == MODIFY)
+                *command &= 0;
 
-        if (perm_type.read)
-        {
-            if (perm_mode == REMOVE)
-                *command &= ~(S_IRUSR | S_IRGRP | S_IROTH);
-            else
-                *command |= S_IRUSR | S_IRGRP | S_IROTH;
-        }
-        if (perm_type.write)
-        {
-            if (perm_mode == REMOVE)
-                *command &= ~(S_IWUSR | S_IWGRP | S_IWOTH);
-            else
-                *command |= S_IWUSR | S_IWGRP | S_IWOTH;
-        }
-        if (perm_type.execute)
-        {
-            if (perm_mode == REMOVE)
-                *command &= ~(S_IXUSR | S_IXGRP | S_IXOTH);
-            else
-                *command |= S_IXUSR | S_IXGRP | S_IXOTH;
-        }
+            if (perm_type.read) {
+                if (perm_mode == REMOVE)
+                    *command &= ~(S_IRUSR | S_IRGRP | S_IROTH);
+                else
+                    *command |= S_IRUSR | S_IRGRP | S_IROTH;
+            }
+            if (perm_type.write) {
+                if (perm_mode == REMOVE)
+                    *command &= ~(S_IWUSR | S_IWGRP | S_IWOTH);
+                else
+                    *command |= S_IWUSR | S_IWGRP | S_IWOTH;
+            }
+            if (perm_type.execute) {
+                if (perm_mode == REMOVE)
+                    *command &= ~(S_IXUSR | S_IXGRP | S_IXOTH);
+                else
+                    *command |= S_IXUSR | S_IXGRP | S_IXOTH;
+            }
 
-        break;
+            break;
 
-    default:
-        break;
+        default:
+            break;
     }
 
     return 0;
 }
 
-int main(int argc, char **argv, char **envp)
-{
-    if (argc < 3)
-    {
-        printf("Usage:\nxmod [OPTIONS] MODE FILE/DIR\nxmod [OPTIONS] OCTAL-MODE FILE/DIR\n");
+int make_command_from_octal_mode(char *mode, unsigned int *command) {
+    if (strlen(mode) != 4 || mode[0] != '0' ||
+        mode[1] < '0' || mode[1] > '7' ||
+        mode[2] < '0' || mode[2] > '7' ||
+        mode[3] < '0' || mode[3] > '7') {
+        printf("ERROR: Invalid octal number. ");
+        printf("Should be a 4-digit number starting with 0.\n");
         return -1;
     }
 
-    if (strlen(argv[1]) < 3 || strlen(argv[1]) > 5)
-    {
-        printf("ERROR: Invalid MODE format. Should be <u|g|o|a><-|+|=><rwx>\n");
+    *command = strtol(mode, &mode, 8);
+
+    return 0;
+}
+
+int main(int argc, char **argv, char **envp) {
+    if (argc < 3) {
+        printf("Usage:\nxmod [OPTIONS] MODE FILE/DIR\n");
+        printf("xmod [OPTIONS] OCTAL-MODE FILE/DIR\n");
+        return -1;
+    }
+
+    if (strlen(argv[1]) < 3 || strlen(argv[1]) > 5) {
+        printf("ERROR: Invalid MODE format. ");
+        printf("Should be <u|g|o|a><-|+|=><rwx> or <-|+|=><0-7><0-7><0-7>\n");
         return -1;
     }
 
     struct stat buffer;
-    if (stat(argv[2], &buffer) != 0)
-    {
+    if (stat(argv[2], &buffer) != 0) {
         perror("ERROR");
         return -1;
     }
@@ -227,14 +225,17 @@ int main(int argc, char **argv, char **envp)
 
     unsigned int command = curr_perm;
 
-    if (make_command_from_text_mode(argv[1], &command) != 0)
-    {
-        printf("Error making new command.\n");
-        return -1;
+    if (isdigit(argv[1][0])) {
+        if (make_command_from_octal_mode(argv[1], &command) != 0) {
+            return -1;
+        }
+    } else {
+        if (make_command_from_text_mode(argv[1], &command) != 0) {
+            return -1;
+        }
     }
 
-    if (chmod(argv[2], command) != 0)
-    {
+    if (chmod(argv[2], command) != 0) {
         perror("chmod() error");
         return -1;
     }
--- MP1/xmod.h
+++ MP1/xmod.h
@@ -1,29 +1,31 @@
+#ifndef MP1_XMOD_H_
+#define MP1_XMOD_H_
+
 /*
 Necessary includes
 */
 
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <stdbool.h>
 
-typedef enum
-{
+typedef enum {
     OWNER,
     GROUP,
     OTHER,
     ALL
 } user_type;
 
-typedef enum
-{
+typedef enum {
     REMOVE,
     ADD,
     MODIFY
 } permission_mode;
 
-typedef struct
-{
+typedef struct {
     bool read;
     bool write;
     bool execute;
@@ -33,3 +35,5 @@ typedef struct
 Responsible for handling the SIGINT signal.
 */
 void sigint_handler(int signumber);
+
+#endif  // MP1_XMOD_H_
