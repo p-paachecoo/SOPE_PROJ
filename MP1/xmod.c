3a4de5c204108d77539d698cde0c0039613d051a
MiNeves00 <miguel.neves.filipe@gmail.com>
2021-03-15 18:19:56 +0000

    Sigint handler now asks if program should terminate

--- MP1/xmod.c
+++ MP1/xmod.c
@@ -5,31 +5,75 @@
    group.
 
  */
-#define _POSIX_SOURCE
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
-#undef _POSIX_SOURCE
-#include <stdio.h>
 
-void main() {
-  char fn[]="temp.txt";
+void sigint_handler(int signumber)
+{
+  fprintf(stderr, "\nReceived signal %d!\n", signumber);
+
+  char answer;
+  int cicle = 0;
+  while (cicle == 0)
+  {
+    printf("Should the program terminate? (y/n)\n");
+    answer = getchar();
+    if (answer == 'y')
+    {
+      printf("Terminated\n");
+      cicle = 1;
+      exit(0);
+    }
+    else if (answer == 'n')
+    {
+      printf("Execution resumed\n");
+      cicle = 1;
+      return;
+    }
+    else
+    {
+      printf("Invalid answer, please answer again (y/n).\n");
+    }
+  }
+}
+
+int main()
+{
+  signal(SIGINT, sigint_handler);
+  struct sigaction new, old;
+  sigset_t smask;                // defines signals to block while func() is running            // prepare struct sigaction
+  if (sigemptyset(&smask) == -1) // block no signal
+    perror("sigsetfunctions");
+
+  new.sa_handler = sigint_handler;
+  new.sa_mask = smask;
+  new.sa_flags = 0; // usually works            if(sigaction(SIGUSR1, &new, &old) == -1)
+  if (sigaction(SIGUSR1, &new, &old) == -1)
+    perror("sigaction");
+  printf("\nSIGUSR1 handler is ready...");
+  printf("\n\t Try: \"sh> kill -SIGUSR1 %d\".\n", getpid());
+
+  pause(); // wait
+  printf("World!\n");
+
+  char fn[] = "temp.txt";
   FILE *stream;
   struct stat info;
 
   if ((stream = fopen(fn, "w+")) == NULL)
     perror("fopen() error");
-  else {
+  else
+  {
     fprintf(stream, "Testing...");
     fclose(stream);
     stat(fn, &info);
     printf("original permissions were: %08x\n", info.st_mode);
-    if (chmod(fn, S_IRWXU|S_IRWXG) != 0)
+    if (chmod(fn, S_IRWXU | S_IRWXG) != 0)
       perror("chmod() error");
-    else {
+    else
+    {
       stat(fn, &info);
       printf("after chmod(), permissions are: %08x\n", info.st_mode);
     }
+    return 0;
     //unlink(fn);
   }
 }
--- MP1/xmod.h
+++ MP1/xmod.h
@@ -2,9 +2,21 @@
 Necessary includes
 */
 
+#define _POSIX_SOURCE
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#undef _POSIX_SOURCE
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
 
-
-
+#define SIGHUP  1   /* Hangup the process */ 
+#define SIGINT  2   /* Interrupt the process */ 
+#define SIGQUIT 3   /* Quit the process */ 
+#define SIGILL  4   /* Illegal instruction. */ 
+#define SIGTRAP 5   /* Trace trap. */ 
+#define SIGABRT 6   /* Abort. */
 
 
 /*
