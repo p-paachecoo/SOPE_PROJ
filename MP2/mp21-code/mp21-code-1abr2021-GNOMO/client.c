891e924b4d00795591e0ab0326380f157648f671
MiNeves00 <miguel.neves.filipe@gmail.com>
2021-05-21 18:32:14 +0100

    Added Their Client

--- MP2/mp21-code/mp21-code-1abr2021-GNOMO/client.c
+++ MP2/mp21-code/mp21-code-1abr2021-GNOMO/client.c
@@ -1,165 +1,258 @@
-#include "client.h"
+/*
+ *	MP2 - client.c - versão 5.mai.2021
+ */
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+#include <pthread.h>
+#include <signal.h>
+#include <dirent.h>
+#include "common.h"
 
-int main(int argc, char *argv[])
-{
-   //Check Args
-   if (argc != 4 || strcmp(argv[1], "-t") != 0)
-   {
-      printf("Usage: c <-t nsecs> fifoname\n");
-      exit(1);
-   }
+#define CONFORTSIZE	1024
+#define PERM 0666	// permissões de acesso ao FIFO
 
-   if (atoi(argv[2]) <= 0)
-   {
-      printf("<-t nsecs> must be greater than 0\n");
-      exit(1);
-   }
+int timeout = 0;				// execution timeout, changed by command line args
+char *serverfifoname = NULL;	// will be kept in command line args
+int serverfifo = -1;			// fifo for incoming requests
 
-   max_time = atoi(argv[2]);
-   initial_time = time(0);
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;	// mutex for accessing the counter (Nuno!)
+int counter = 0;				// counter of launched threads, serves as unique request id
+int finish = 0;					// whether time to finish
 
-   server_path = argv[3];
-   do
-   {
-      fd_server = open(server_path, O_WRONLY);
-      if (fd_server == -1)
-      {
-         printf("Connecting to server ...\n");
-         sleep(1);
-      }
-   } while (fd_server == -1 && difftime(time(0), initial_time) < max_time);
+void get_opt(int , char* []);
+void alrm(int);
+void pips(int);
+void terminate_blocked (pid_t);
+void cleanup_handler(void *);
 
-   if (pthread_mutex_init(&lock1, NULL) != 0)
-   {
-      printf("\n mutex init failed\n");
-      return 1;
-   }
+// thread that launches a request to the server and waits for reply
+void* thread_client(void* arg) {
+		int clientfifo = -1;
+		char clientfifoname[CONFORTSIZE];
+	// create a unique fifo to get back the answer
+	sprintf(clientfifoname, "/tmp/%d.%lu", getpid(), (unsigned long) pthread_self());
+	if (mkfifo(clientfifoname, PERM) < 0)  {
+		perror ("mkfifo clientfifoname");
+		pthread_exit(NULL);
+	}
+	fprintf(stderr,"[client] created client fifo %s\n",clientfifoname);
 
-   if (pthread_mutex_init(&lock2, NULL) != 0)
-   {
-      printf("\n mutex init failed\n");
-      return 1;
-   }
+	// generate unique request id
+	pthread_mutex_lock(&mut);
+	int	rid = counter++;
+	pthread_mutex_unlock(&mut);
+	// generates a random task load
+	int t = (rand() % 9) + 1;
 
-   //Launch Cn request threads
-   time_t t;
-   unsigned int seed = (unsigned)time(&t);
-   srand(seed);
+	Message *request = malloc(sizeof(Message));
+	if (request == NULL) {
+		perror ("malloc(sizeof(Message)");
+		unlink(clientfifoname);
+		pthread_exit(NULL);
+	}
 
-   while (difftime(time(0), initial_time) < max_time)
-   {
-      if (!server_closed)
-         createRequests();
+	request->rid = rid;
+	request->tskload = t;
+	request->pid = getpid();
+	request->tid = pthread_self();
+	request->tskres = -1;
 
-      usleep((rand_r(&seed) % 50 + 30) * 1000); //sleep between 30 and 80ms
-   }
+	// serverfifo was opened in main() ; write() should not block...
+	if (write(serverfifo,request,sizeof(Message)) < 0) {
+		perror("[client] write serverfifo");
+		free(request);
+		unlink(clientfifoname);
+		pthread_exit(NULL);
+	}
 
-   close(fd_server);
+	printf("%ld ; %d ; %d ; %d ; %lu ; %d; IWANT\n", time(NULL), request->rid, request->tskload, request->pid, (unsigned long) request->tid, request->tskres);
 
-   return 0;
-}
+ 	pthread_cleanup_push(cleanup_handler, request);	// for the case of cancelling
 
-//Create Requests Thread -> C0
-void createRequests()
-{
-   int err;
-   pthread_t id;
+	if((clientfifo = open(clientfifoname, O_RDONLY)) < 0) {	// open blocks; break it by cancelling the thread
+		perror ("open clientfifo");
+		printf("%ld ; %d ; %d ; %d ; %lu ; %d; GAVUP\n", time(NULL), request->rid, request->tskload, request->pid, (unsigned long) request->tid, request->tskres);
+		free(request);
+		unlink(clientfifoname);
+		return (NULL);
+	}
 
-   if ((err = pthread_create(&id, NULL, makeRequest, NULL)) != 0)
-   {
-      fprintf(stderr, "C0 thread: %s!\n", strerror(err));
-      exit(-1);
-   }
-}
+ 	fprintf(stderr,"[client] opened client fifo %s\n", clientfifoname);
 
-//Request Threads -> Cn
-void *makeRequest()
-{
+		int nread;
+		Message answer;
+	if ((nread = read(clientfifo, (void *)&answer, sizeof(Message))) < 0) {	// read blocks; break it by cancelling the thread
+		perror("[client] read(clientfifo)");
+		printf("%ld ; %d ; %d ; %d ; %lu ; %d; GAVUP\n", time(NULL), request->rid, request->tskload, request->pid, (unsigned long) request->tid, request->tskres);
+		}
+	else if (nread == 0) {	// server closed private FIFO
+		fprintf(stderr,"[client] server closed client fifo %s\n", clientfifoname);
+		printf("%ld ; %d ; %d ; %d ; %lu ; %d; GAVUP\n", time(NULL), request->rid, request->tskload, request->pid, (unsigned long) request->tid, request->tskres);
+		}
+	else if (answer.tskres < 0) {
+		printf("%ld ; %d ; %d ; %d ; %lu ; %d; CLOSD\n", time(NULL), request->rid, request->tskload, request->pid, (unsigned long) request->tid, answer.tskres);
+		}
+	else {
+		printf("%ld ; %d ; %d ; %d ; %lu ; %d; GOTRS\n", time(NULL), request->rid, request->tskload, request->pid, (unsigned long) request->tid, answer.tskres);
+		}
 
-   //Create Private FIFO
-   char client_fifo[256];
-   snprintf(client_fifo, sizeof(client_fifo), "/tmp/%d.%ld", getpid(), pthread_self());
+ 	pthread_cleanup_pop(0);	// disable cancelling functions
+	free(request);
+	close(clientfifo);
+	unlink(clientfifoname);
+	return(NULL);
+} // thread_client()
 
-   if (mkfifo(client_fifo, 0666) < 0)
-   {
-      perror("mkfifo");
-   }
+int main(int argc, char** argv) {
+	get_opt(argc,argv);
 
-   int fd_client;
-   fd_client = open(client_fifo, O_RDONLY | O_NONBLOCK);
+	if (timeout == 0) {
+		fprintf(stderr, "[server] no timeout set - exiting\n");
+		exit(1);
+	}
+		struct sigaction new, old;
+		sigset_t smask;	// signals to mask during signal handler
+	sigemptyset(&smask);
+	new.sa_handler = alrm;
+	new.sa_mask = smask;
+	new.sa_flags = 0;	// usually enough
+	if(sigaction(SIGALRM, &new, &old) == -1) {
+		perror ("sigaction (SIGALRM)");
+		exit(2);
+	}
+		struct sigaction new2;
+	new2.sa_handler = pips;
+	new2.sa_mask = smask;
+	new2.sa_flags = 0;	// usually enough
+	if(sigaction(SIGPIPE, &new2, NULL) == -1) {
+		perror ("sigaction (SIGPIPE)");
+		exit(2);
+	}
 
-   time_t t;
+	alarm(timeout);	// this will flip the "finish" flag
 
-   unsigned int seed = (unsigned)(time(&t) + pthread_self() % 100);
-   srand(seed);
-   int task = rand_r(&seed) % 8 + 1; //1-9 inclusive
+	while ((serverfifo = open(serverfifoname, O_WRONLY)) < 0) {	// 1st time: keep blocking until server opens...
+		perror("[client] client, open serverfifo");
+		if (finish)	// client timeout!
+			pthread_exit(NULL);
+	}
 
-   struct message *msg = &(struct message){
-       .rid = identifier_c,
-       .pid = getpid(),
-       .tid = pthread_self(),
-       .tskload = task,
-       .tskres = -1};
+	srand(time(NULL));	// seed the task "loads"
+	pthread_t tid;	// temporary, for any of the client threads
 
-   pthread_mutex_lock(&lock1);
-   identifier_c += 1;
-   pthread_mutex_unlock(&lock1);
+	// launches requests until client times out
+	while (1) {
 
-   if (difftime(time(0), initial_time) >= max_time)
-   {
-      close(fd_client);
-      unlink(client_fifo);
+		while (pthread_create(&tid, NULL, thread_client, NULL) != 0) {	// wait till a new thread can be created!
+			perror("[client] server thread");
+			usleep(10000 + (rand() % 10000));
+			if (finish)	// client timeout!
+				goto timetoclose;
+		}
+		// sleeps a random time until next request
+		usleep(10000 + (rand() % 10000));	// prepare new request
+// 		usleep(100 + (rand() % 100));	// good for forcing 2LATEs !
 
-      pthread_exit(NULL);
-   }
+		if (access(serverfifoname, F_OK) < 0) { // if server removed public FIFO
+			close (serverfifo);
+			do { // busy wait until server re-opens or client times out
+				if (finish)	// client timeout!
+					goto timetoclose;
+			} while (access(serverfifoname, F_OK) < 0);	// busy wait until server re-opens or client times out
+			// server should have re-opened
+			if ((serverfifo = open(serverfifoname, O_WRONLY)) < 0) {	// keep blocking until server opens... OR intr (e.g. alarm) occurs
+				perror("[client] client, open serverfifo");
+				goto timetoclose;	// could be error or timeout
+			} // open(serverfifoname)
+		} // if(access())
 
-   pthread_mutex_lock(&lock2);
+		if (finish)	// client timeout!
+			goto timetoclose;
+	} // while(1)
 
-   write(fd_server, msg, sizeof(*msg));
+timetoclose:
+	fprintf(stderr, "[client] stopped creating requests\n");
+	// strategy: 1 2
+	// 1 - break all blocked threads with pthread_cancel() and assure GAVEUP messages
+		// como saber os thr_ids? percorrendo /tmp/[pid].* !
+	terminate_blocked(getpid());
+	// 2 - assure that all private FIFOs are removed
+	char rm[256] = {'\0'};
+	snprintf (rm, sizeof (rm), "rm -rf /tmp/%d.*", getpid());
+	system (rm);
 
-   pthread_mutex_unlock(&lock2);
+	fprintf(stderr, "[client] main terminating\n");
+	pthread_exit(NULL);
+} // main()
 
-   log_msg(msg->rid, msg->pid, msg->tid, msg->tskload, msg->tskres, "IWANT");
+void get_opt(int argc, char* argv[]) {
+	if(argc < 3) {
+		fprintf(stderr, "Usage: %s <-t nsecs> <fifoname>\n", argv[0]);
+		exit(1);
+	}
+	int opt;
+	while ((opt = getopt(argc, argv, "t:")) != -1) {
+		switch (opt) {
+			case 't':
+				timeout = atoi(optarg);
+				fprintf(stderr, "Initial time: %lu, expected final time: %lu\n",time(NULL),time(NULL)+timeout);
+				break;
+			default:
+				fprintf(stderr, "Usage: %s <-t nsecs> <fifoname>\n", argv[0]);
+				exit(EXIT_FAILURE);
+		}
+	}
+		if (optind != argc-1) {
+		fprintf(stderr, "Usage: %s <-t nsecs> <fifoname>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	else
+		serverfifoname = argv[optind];
+	fprintf(stderr,"\nGot: nsecs=%d, fifoname=%s\n", timeout, serverfifoname);
+} // get_opt
 
-   signal(SIGPIPE, SIG_IGN);
+void alrm(int signo) {
+	finish = 1;
+	fprintf(stderr, "[client] timeout reached: %ld %ld\n", time(NULL), (unsigned long) pthread_self());
+} // alrm()
 
-   struct message msg_received;
-   int timeout = 0;
-   while (read(fd_client, &msg_received, sizeof(msg_received)) <= 0)
-   {
-      if (difftime(time(0), initial_time) >= max_time)
-      {
-         timeout = 1;
-         break;
-      }
-      usleep(10000);
-   }
-   if (timeout == 0)
-   { //received msg
-      if (msg_received.tskres == -1)
-      {
-         log_msg(msg->rid, getpid(), pthread_self(), msg_received.tskload, msg_received.tskres, "CLOSD");
-         //server_closed = 1;
-      }
-      else
-         log_msg(msg->rid, getpid(), pthread_self(), msg_received.tskload, msg_received.tskres, "GOTRS");
-   }
-   else
-      log_msg(msg->rid, getpid(), pthread_self(), msg->tskload, msg->tskres, "GAVUP");
+void pips(int signo) {
+	finish = 1;
+	fprintf(stderr, "[client] server pipe closed\n");
+} // pips()
 
-   close(fd_client);
-   unlink(client_fifo);
+void terminate_blocked (pid_t pid) {
+		DIR *od;
+		struct dirent *ds;
+		char fifoname [1024] = {'\0'};
+		char threadid [1024] = {'\0'};
+		pthread_t tid;
+	sprintf(fifoname, "%d.", pid);
 
-   pthread_exit(NULL);
-}
+	if ((od = opendir("/tmp")) == NULL) {
+		perror("opendir");
+	}
+	while((ds = readdir(od)) != NULL) {
+		char *ptr;
+		if ((ptr = strstr(ds->d_name, fifoname)) != NULL) {
+			ptr = ptr + strlen(fifoname);
+			sprintf(threadid, "%s", ptr);
+			tid = (unsigned long) atoll(threadid);
+			pthread_cancel(tid);
+			fprintf(stderr, "[client] cancelled thread %lu.\n", tid);
+		}
+	}
+} //terminate_blocked()
 
-void log_msg(int rid, pid_t pid, pthread_t tid, int tskload, int tskres, char *operation)
-{
-   char msg[128];
-   time_t inst = time(NULL);
-
-   snprintf(msg, sizeof(msg), "%ld ; %d ; %d ; %d ; %ld ; %d ; %s\n", inst, rid, tskload, pid, tid, tskres, operation);
-
-   if (msg != NULL)
-      write(STDOUT_FILENO, msg, strlen(msg));
-}
+void cleanup_handler(void *arg) {
+		Message *request = (Message *) arg;
+	fprintf(stderr, "[Client] Called clean-up handler\n");
+	printf("%ld ; %d ; %d ; %d ; %lu ; %d; GAVUP\n", time(NULL), request->rid, request->tskload, request->pid, (unsigned long) request->tid, request->tskres);
+} // cleanup_handler()
---
+++ MP2/mp21-code/mp21-code-1abr2021-GNOMO/client_nosso.c
@@ -0,0 +1,165 @@
+#include "client.h"
+
+int main(int argc, char *argv[])
+{
+   //Check Args
+   if (argc != 4 || strcmp(argv[1], "-t") != 0)
+   {
+      printf("Usage: c <-t nsecs> fifoname\n");
+      exit(1);
+   }
+
+   if (atoi(argv[2]) <= 0)
+   {
+      printf("<-t nsecs> must be greater than 0\n");
+      exit(1);
+   }
+
+   max_time = atoi(argv[2]);
+   initial_time = time(0);
+
+   server_path = argv[3];
+   do
+   {
+      fd_server = open(server_path, O_WRONLY);
+      if (fd_server == -1)
+      {
+         printf("Connecting to server ...\n");
+         sleep(1);
+      }
+   } while (fd_server == -1 && difftime(time(0), initial_time) < max_time);
+
+   if (pthread_mutex_init(&lock1, NULL) != 0)
+   {
+      printf("\n mutex init failed\n");
+      return 1;
+   }
+
+   if (pthread_mutex_init(&lock2, NULL) != 0)
+   {
+      printf("\n mutex init failed\n");
+      return 1;
+   }
+
+   //Launch Cn request threads
+   time_t t;
+   unsigned int seed = (unsigned)time(&t);
+   srand(seed);
+
+   while (difftime(time(0), initial_time) < max_time)
+   {
+      if (!server_closed)
+         createRequests();
+
+      usleep((rand_r(&seed) % 50 + 30) * 1000); //sleep between 30 and 80ms
+   }
+
+   close(fd_server);
+
+   return 0;
+}
+
+//Create Requests Thread -> C0
+void createRequests()
+{
+   int err;
+   pthread_t id;
+
+   if ((err = pthread_create(&id, NULL, makeRequest, NULL)) != 0)
+   {
+      fprintf(stderr, "C0 thread: %s!\n", strerror(err));
+      exit(-1);
+   }
+}
+
+//Request Threads -> Cn
+void *makeRequest()
+{
+
+   //Create Private FIFO
+   char client_fifo[256];
+   snprintf(client_fifo, sizeof(client_fifo), "/tmp/%d.%ld", getpid(), pthread_self());
+
+   if (mkfifo(client_fifo, 0666) < 0)
+   {
+      perror("mkfifo");
+   }
+
+   int fd_client;
+   fd_client = open(client_fifo, O_RDONLY | O_NONBLOCK);
+
+   time_t t;
+
+   unsigned int seed = (unsigned)(time(&t) + pthread_self() % 100);
+   srand(seed);
+   int task = rand_r(&seed) % 8 + 1; //1-9 inclusive
+
+   struct message *msg = &(struct message){
+       .rid = identifier_c,
+       .pid = getpid(),
+       .tid = pthread_self(),
+       .tskload = task,
+       .tskres = -1};
+
+   pthread_mutex_lock(&lock1);
+   identifier_c += 1;
+   pthread_mutex_unlock(&lock1);
+
+   if (difftime(time(0), initial_time) >= max_time)
+   {
+      close(fd_client);
+      unlink(client_fifo);
+
+      pthread_exit(NULL);
+   }
+
+   pthread_mutex_lock(&lock2);
+
+   write(fd_server, msg, sizeof(*msg));
+
+   pthread_mutex_unlock(&lock2);
+
+   log_msg(msg->rid, msg->pid, msg->tid, msg->tskload, msg->tskres, "IWANT");
+
+   signal(SIGPIPE, SIG_IGN);
+
+   struct message msg_received;
+   int timeout = 0;
+   while (read(fd_client, &msg_received, sizeof(msg_received)) <= 0)
+   {
+      if (difftime(time(0), initial_time) >= max_time)
+      {
+         timeout = 1;
+         break;
+      }
+      usleep(10000);
+   }
+   if (timeout == 0)
+   { //received msg
+      if (msg_received.tskres == -1)
+      {
+         log_msg(msg->rid, getpid(), pthread_self(), msg_received.tskload, msg_received.tskres, "CLOSD");
+         //server_closed = 1;
+      }
+      else
+         log_msg(msg->rid, getpid(), pthread_self(), msg_received.tskload, msg_received.tskres, "GOTRS");
+   }
+   else
+      log_msg(msg->rid, getpid(), pthread_self(), msg->tskload, msg->tskres, "GAVUP");
+
+   close(fd_client);
+   unlink(client_fifo);
+
+   pthread_exit(NULL);
+}
+
+void log_msg(int rid, pid_t pid, pthread_t tid, int tskload, int tskres, char *operation)
+{
+   char msg[128];
+   time_t inst = time(NULL);
+
+   snprintf(msg, sizeof(msg), "%ld ; %d ; %d ; %d ; %ld ; %d ; %s\n", inst, rid, tskload, pid, tid, tskres, operation);
+
+   if (msg != NULL)
+      write(STDOUT_FILENO, msg, strlen(msg));
+}
